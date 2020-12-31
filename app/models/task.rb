87e860f2526c319fca712c47c3072c5a5de38aee
Karn Tirasoontorn <karn@hey.com>
2020-12-31 21:42:39 +0700

    Show empty message if task is 0

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,21 @@
 class Task < ApplicationRecord
-  after_create_commit -> { broadcast_append_to :tasks, partial: 'tasks/task', locals: { task: self } }
+  after_create_commit :render_for_create
   after_update_commit -> { broadcast_replace_to :tasks, target: "#{self.class.name.downcase}_#{id}", partial: 'tasks/task', locals: { task: self } }
-  after_destroy_commit -> { broadcast_action_to :tasks, action: "remove", target: "#{self.class.name.downcase}_#{id}" }
+  after_destroy_commit :render_for_destroy
+
+  def render_for_create
+    if Task.all.size == 1
+      broadcast_replace_to :tasks, target: :tasks, partial: 'tasks/turbo_task', locals: { task: self }
+    else
+      broadcast_append_to :tasks, partial: 'tasks/task', locals: { task: self }
+    end
+  end
+
+  def render_for_destroy
+    if Task.all.size.zero?
+      broadcast_replace_to :tasks, target: :tasks, partial: 'tasks/list', locals: { tasks: [] }
+    else
+      broadcast_action_to :tasks, action: "remove", target: "#{self.class.name.downcase}_#{id}"
+    end
+  end
 end
